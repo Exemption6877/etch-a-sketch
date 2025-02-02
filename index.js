73d0c0249c3a7a18066c7c82477e337f09b4118b
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-02 19:49:59 +0300

    Implement grid size change function

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ function createGrid(num) {
   }
 }
 
-createGrid(64);
+function removeGrid() {
+  const squares = document.querySelectorAll(".square");
+  const lines = document.querySelectorAll(".line");
+
+  squares.forEach((element) => {
+    element.remove();
+  });
+
+  lines.forEach((element) => {
+    element.remove();
+  });
+}
+
+createGrid(16);
 
 const reset = document.querySelector("#reset");
 
@@ -27,3 +40,21 @@ reset.addEventListener("click", (event) => {
   });
 });
 
+const size = document.querySelector("#size");
+
+size.addEventListener("click", () => {
+  let response = parseInt(prompt("Enter your size under 100: "));
+
+  if (response > 100) {
+    response = parseInt(
+      prompt("The number is too big, please enter the number below 100: ")
+    );
+  } else if (response < 1) {
+    response = parseInt(
+      prompt("The number is too low, please enter the bigger than 0: ")
+    );
+  }
+
+  removeGrid();
+  createGrid(response);
+});
